5519e8e9b0511d1f82eb12e001730fbffef5c5f8
veeresh shetty <kmveereshshetty@gmail.com>
2018-06-26 10:41:02 -0400

    user defined exception

---
+++ veer1/src/veer1/Myownexception.java
@@ -0,0 +1,27 @@
+package veer1;
+import java.util.*;
+public class Myownexception {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			System.out.println("Please enter a month value\n");
+			Scanner p1 = new Scanner(System.in);
+			int num = p1.nextInt();
+			if(num < 0 || num > 12)
+			throw new InvalidMonthException("Invalid Month Value");
+		}
+		catch (Exception e)
+		{
+		e.printStackTrace();
+		}
+
+	}
+
+}
+ class  InvalidMonthException extends Exception {
+	
+	 InvalidMonthException(String msg){
+		 super(msg);
+	 }
+ }
